2ec5ef8fc28794281a8bc24421c0bdb6fde07632
sweetblasphemy <nik2006@abv.bg>
2023-06-17 10:59:01 +0300

    Add project files.

---
+++ uchenici.po.fizichesko/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace uchenici.po.fizichesko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int[] heights = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                int[] positions = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+                
+                int height1 = heights[positions[0] - 1];
+                int height2 = heights[positions[1] - 1];
+
+              
+                Console.WriteLine($"{height1} {height2}");
+
+        }
+    }
+}
